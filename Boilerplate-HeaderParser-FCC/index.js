d65300dc5f990a7e90bc34caacad1479ddeed088
younelma <youne.elma@gmail.com>
2024-02-15 17:23:36 +0100

    Add the second Boilerplate-HeaderParser-FCC project

---
+++ Boilerplate-HeaderParser-FCC/index.js
@@ -0,0 +1,44 @@
+// index.js
+// where your node app starts
+
+// init project
+require("dotenv").config();
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/api/whoami", (req, res) => {
+  const ipAdress = req.ip;
+  const language = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+
+  const response = {
+    ipaddress: ipAdress,
+    language: language,
+    software: software,
+  };
+
+  res.json(response);
+});
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
