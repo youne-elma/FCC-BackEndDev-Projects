5912187db35cd798c7986bee3a0abf9ea6a4c011
younelma <youne.elma@gmail.com>
2024-02-16 16:03:29 +0100

    Add: Integrate bodyParser middleware for request body parsing

--- Boilerplate-UrlShortner-FCC/index.js
+++ Boilerplate-UrlShortner-FCC/index.js
@@ -1,24 +1,30 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl", (req, res) => {});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
